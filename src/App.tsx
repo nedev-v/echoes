3d764539981f8e80f2e4a1b87eb8624914d31642
Oleksandr Nediev <sashaned2000@gmail.com>
2025-07-12 22:32:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { RecordButton } from './screens/HomeScreen.tsx/shared/components/RecordButton'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-[600px] h-screen mx-auto flex justify-center items-center'>
+        <RecordButton />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/screens/HomeScreen.tsx/shared/hooks/useRecorder.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useRef, useState } from "react";
+
+export function useRecorder(mimeHint = "audio/webm;codecs=opus") {
+    const mediaRecRef = useRef<MediaRecorder | undefined>(undefined);
+    const chunksRef = useRef<Blob[]>([]);
+    const [status, setStatus] = useState<string>("inactive");
+    const [url, setUrl] = useState<string | null>(null);
+
+    const start = useCallback(async () => {
+        if (status === "recording") return;
+
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mime = MediaRecorder.isTypeSupported(mimeHint) ? mimeHint : "";
+
+        const mediaRec = new MediaRecorder(stream, { mimeType: mime });
+        mediaRecRef.current = mediaRec;
+        chunksRef.current = [];
+
+        mediaRec.ondataavailable = (e) => e.data.size > 0 && chunksRef.current.push(e.data);
+
+        mediaRec.onstop = () => {
+            const blob = new Blob(chunksRef.current, { type: mime });
+            setUrl(URL.createObjectURL(blob));
+        }
+        
+        mediaRec.start();
+        setStatus("recording");
+    }, [mimeHint, status]);
+
+    const stop = useCallback(() => {
+        if (status !== "recording") return;
+        mediaRecRef.current?.stop();
+        setStatus("inactive");
+    }, [status]);
+
+    return({ start, stop, status, url });
+}
+
